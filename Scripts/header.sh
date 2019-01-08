33d6758e0bfda60cc2bbe93abbd6eabc087ebfe9
meilier <1967562123@qq.com>
2019-01-08 15:42:40 +0800

    add config

--- Scripts/header.sh
+++ Scripts/header.sh
@@ -1,3 +1,21 @@
-export CAPATH="/Users/xingweizheng/testecc"
-export CLIENTPATH="/Users/xingweizheng/client"
-export WORKDIR="/Users/xingweizheng/github/pca"
+WORKDIR="/Users/xingweizheng/github/pca"
+CONFIG=$WORKDIR/Config/config.cfg
+
+# if config file enable?
+source $CONFIG
+echo "enable config ?" $ENABLECONFIG
+if [ $ENABLECONFIG -eq 0 ]
+then
+# set var to default
+CAPATH="/Users/xingweizheng/testecc"
+CLIENTPATH="/Users/xingweizheng/client"
+CAPASS=123456
+CLIENTPASS=123456
+COUNTRYNAME=CN
+STATEPROVINCENAME=ZHEJIANG
+ORGNAME=ZJU
+ORGUNITNAME=ZJUCS
+COMMONNAMECA=CA
+COMMONNAMEACCOUNT=ACCOUNT
+COMMONNAMETLS=TLS
+fi
--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -16,9 +16,9 @@ echo '00' > $CAPATH/serial
 echo "set up directoty ok" 
 
 # create ca root private key and pem
-openssl ecparam -name secp384r1 -genkey | openssl ec -aes-256-cbc -passout pass:"123456" -out $CAPATH/private/cakey.pem
+openssl ecparam -name secp384r1 -genkey | openssl ec -aes-256-cbc -passout pass:$CAPASS -out $CAPATH/private/cakey.pem
 chmod 400 $CAPATH/private/cakey.pem
-openssl req -config $CAPATH/openssl.cnf -key $CAPATH/private/cakey.pem -new -extensions ext_root -out $CAPATH/private/cacert.pem -x509 -passin pass:"123456" -subj '/C=US/ST=California/O=XXXX/OU=XXXX Certificate Authority/CN=XXXX Root CA' -days 7300
+openssl req -config $CAPATH/openssl.cnf -key $CAPATH/private/cakey.pem -new -extensions ext_root -out $CAPATH/private/cacert.pem -x509 -passin pass:$CAPASS -subj /C=$COUNTRYNAME/ST=$STATEPROVINCENAME/O=$ORGNAME/OU=$ORGUNITNAME/CN=$COMMONNAMECA -days 7300
 chmod 444 $CAPATH/private/cacert.pem
 echo "generate ca file ok"
 
